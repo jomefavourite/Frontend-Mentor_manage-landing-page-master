d3289795d26b81d0362585a5c31a78b4c527d606
jomefavourite <54733225+jomefavourite@users.noreply.github.com>
2020-07-12 00:01:40 +0100

    code

---
+++ main.js
@@ -0,0 +1,34 @@
+function call(name) {
+    return document.querySelector(name)
+}
+
+let ham = call('.hamburger')
+let overlay = call('.overlay')
+let nav = call('.header__Nav')
+let input = call('input')
+let button = call('button[type=submit]')
+let alertt = call('.alert')
+
+ham.addEventListener('click', () => {
+    overlay.classList.toggle('toggle_show');
+    nav.classList.toggle('toggle_show')
+    ham.classList.toggle('change')
+})
+
+button.addEventListener('click', (e) => {
+    e.preventDefault()
+    if (input.value.length > 0) {
+        if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(input.value)) {
+            alertt.textContent = ""
+            console.log("hi");
+        } 
+        else {
+            alertt.textContent = `Please insert a valid email`
+            // input.value.classList.add('red')
+        }
+    } else {
+        alertt.textContent = ""
+    }
+    console.log(input.value.setAttribute('class', 'red'));
+    
+})
